f835fd302642561bf228dcd6c7f12660b3c5ed03
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-31 22:50:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchies][:languages]
+		 programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
